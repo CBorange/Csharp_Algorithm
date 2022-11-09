1e64beebdbc65c07dc4306a2e45326b14507f412
LTJ <xocnsdl2013@gmail.com>
2022-11-09 17:28:13 +0900

    - 햄버거 만들기 솔루션 Class 추가

--- Algorithm/MakeHamburger_Solution.cs
+++ Algorithm/MakeHamburger_Solution.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace Algorithm
 {
+    /// <summary>
+    /// 프로그래머스 코테 LV.1 [햄버거 만들기] 솔루션
+    /// </summary>
     public class MakeHamburger_Solution
     {
         public int solution(int[] ingredient)
         {
+
             int answer = 0;
             return answer;
         }
